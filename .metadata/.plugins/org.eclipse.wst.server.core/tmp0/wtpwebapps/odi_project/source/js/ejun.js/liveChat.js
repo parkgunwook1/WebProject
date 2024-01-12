21ec9f2814e6d05f22363ae1b0b60cda02b51701
parkgunwook1 <pkw85428@naver.com>
2024-01-12 09:35:36 +0900

    회원(CRUD)

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/odi_project/source/js/ejun.js/liveChat.js
@@ -0,0 +1,10 @@
+
+// chatPage
+function createChatRoom(){
+  
+  if(document.querySelector("#create-chat-box").style.display=="none"){
+    document.querySelector("#create-chat-box").style.display="block"
+
+  }else
+    document.querySelector('#create-chat-box').style.display="none"
+}
